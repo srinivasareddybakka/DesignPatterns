12f440c1f09387d0da190414645d0c7f6253feb7
srinivasareddybakka11 <srinivasareddybakka@gmail.com>
2020-07-18 08:29:13 +0530

    Factory Design Pattern Initial commit.

---
+++ DesignPatternsPrograms/src/com/designpatterns/factorydesign/FactoryDesign.java
@@ -0,0 +1,7 @@
+package com.designpatterns.factorydesign;
+
+public class FactoryDesign {
+    public static void main(String[] args) {
+        System.out.println("Main method in Factroy Design");
+    }
+}
